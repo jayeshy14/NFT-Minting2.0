2ecb555655409cf79a834190837e2fc2c5c65543
Jayesh Yadav <jayeshnyadav497@gmail.com>
2024-08-07 11:11:42 +0530

    updated components

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -142,11 +142,11 @@ function Create({marketplace}) {
 
   <div className="mb-4">
     <label htmlFor="price" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Enter Price</label>
-    <input onChange={handleChange} type="text" id="price" name='price' className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="Enter price" />
+    <input onChange={handleChange} type="text" id="price" name='price' className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="Enter price" required/>
   </div>
 
   <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
-  <textarea onChange={handleChange}  name="description" id="description" rows="4" className="block p-2.5 w-full text-sm  mb-4 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
+  <textarea onChange={handleChange}  name="description" id="description" rows="4" className="block p-2.5 w-full text-sm  mb-4 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write description about NFT..."></textarea>
  
  <div className='text-center'>
 
